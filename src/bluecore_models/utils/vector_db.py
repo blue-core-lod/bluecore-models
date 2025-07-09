a7c79104cc796d85f4b2d47e7d3d553dbeed0bac
Jeremy Nelson <jermnelson@gmail.com>
2025-07-08 18:07:27 -0700

    feat: create_embeddings function and test

--- src/bluecore_models/utils/vector_db.py
+++ src/bluecore_models/utils/vector_db.py
@@ -1,6 +1,7 @@
 import logging
 import os
 
+from typing import Union
 
 from pymilvus import model, MilvusClient
 
@@ -9,7 +10,7 @@ from bluecore_models.models import Version
 
 logger = logging.getLogger(__name__)
 
-MILVUS_URI = os.environ.get("MILVUS_URI", "http://localhost:19530")
+MILVUS_URI = os.environ.get("MILVUS_URI")
 
 
 def init_collections(client: MilvusClient):
@@ -25,3 +26,47 @@ def init_collections(client: MilvusClient):
             collection_name="instances",
             dimension=768,
         )
+
+
+def create_embeddings(
+    version: Version, collection: str, client: Union[MilvusClient, None] = None
+):
+    if not client:
+        client = MilvusClient(uri=MILVUS_URI)
+
+    init_collections(client)
+
+    embedding_func = model.DefaultEmbeddingFunction()
+
+    version_graph = init_graph()
+    version_graph.parse(data=version.data, format="json-ld")
+    version_id = version.id
+
+    resource_uri = version.resource.uri
+    skolemized_graph = version_graph.skolemize(basepath=f"{resource_uri}#")
+
+    triples = [
+        line.rstrip(".")
+        for line in skolemized_graph.serialize(format="nt").splitlines()
+    ]
+
+    triple_vectors = embedding_func.encode_documents(triples)
+
+    embeddings_data = []
+
+    for i, vector in enumerate(triple_vectors):
+        embeddings_data.append(
+            {
+                "id": i,
+                "vector": vector,
+                "text": triples[i],
+                "uri": resource_uri,
+                "version": version_id,
+            }
+        )
+
+    logging.info(
+        f"Creating embeddings for {resource_uri} version {version_id}, total vectors: {len(triple_vectors)}"
+    )
+    result = client.insert(collection_name=collection, data=embeddings_data)
+    logging.info(f"Inserted {result['insert_count']} triple embeddings")
--- tests/test_vector_db.py
+++ tests/test_vector_db.py
@@ -1,8 +1,9 @@
-import pytest
+import pytest  # noqa
 
 from pymilvus import MilvusClient
 
-from bluecore_models.utils.vector_db import init_collections
+from bluecore_models.models import Version
+from bluecore_models.utils.vector_db import create_embeddings, init_collections
 
 
 def test_init_collections(tmp_path):
@@ -30,4 +31,16 @@ def test_init_collections_existing(tmp_path, caplog):
 
     init_collections(test_client)
 
-    assert not "Creating works collection" in caplog.text
+    assert "Creating works collection" not in caplog.text
+
+
+def test_create_embeddings(pg_session, tmp_path):
+    test_db_path = tmp_path / "vector-test.db"
+    test_client = MilvusClient(str(test_db_path))
+
+    with pg_session() as session:
+        version = session.query(Version).where(Version.id == 1).first()
+        create_embeddings(version, "works", test_client)
+
+    collection_stats = test_client.get_collection_stats("works")
+    assert collection_stats["row_count"] == 79
