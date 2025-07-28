4b3892f8b9f5d5774fa56b3236d3fde3c91ee7cc
Ed Summers <ehs@pobox.com>
2025-07-28 11:51:57 -0400

    Prevent duplicate versions (alternative fix) (#62)
    * bug: Checks for existing versions before add new version
    
    * feat: Adds test that replicates CBD ingestion
    
    * feat: Updates models version
    
    * refactor: Fixes lint error
    
    * Prevent duplicate versions
    
    This is a branch of the `t57-version-fix` branch in #60 which provides alternate logic for determining whether to create a new version using a technique suggested in the [SQLAlchemy docs](https://github.com/blue-core-lod/bluecore-models/pull/60/commits/83648d8dc83194f9bce5ace82421782f26cb25af):
    
    > This method is called for all instances that are marked as “dirty”, even those which have no net changes to their column-based attributes, and for which no UPDATE statement has proceeded. An object is marked as dirty when any of its column-based attributes have a “set attribute” operation called or when any of its collections are modified. If, at update time, no column-based attributes have any net changes, no UPDATE statement will be issued. This means that an instance being sent to MapperEvents.after_update() is not a guarantee that an UPDATE statement has been issued.
    >
    > To detect if the column-based attributes on the object have net changes, and therefore resulted in an UPDATE statement, use object_session(instance).is_modified(instance, include_collections=False).
    
    It also introduces a commit after the new Work is created in the new test. Without this, the new version will never get created even if there is no logic in place to prevent it.
    
    Fixes #57
    
    ---------
    
    Co-authored-by: Jeremy Nelson <jermnelson@gmail.com>

--- src/bluecore_models/utils/db.py
+++ src/bluecore_models/utils/db.py
@@ -4,8 +4,10 @@ from typing import Optional
 
 import rdflib
 from sqlalchemy import delete, insert, select
+from sqlalchemy.orm import object_session
 
 from bluecore_models.models.bf_classes import BibframeClass, ResourceBibframeClass
+from bluecore_models.models.version import Version
 from bluecore_models.utils.graph import get_bf_classes, frame_jsonld
 
 
@@ -28,6 +30,19 @@ def add_bf_classes(connection, resource):
             bf_class_id = _new_bf_classs(connection, class_)
         stmt = insert(ResourceBibframeClass.__table__).values(
             bf_class_id=bf_class_id, resource_id=resource.id
+        )
+        connection.execute(stmt)
+
+
+def add_version(connection, resource):
+    """
+    Adds a Version if the resource had been modified.
+    """
+    if object_session(resource).is_modified(resource, include_collections=False):
+        stmt = insert(Version.__table__).values(
+            resource_id=resource.id,
+            data=resource.data,
+            created_at=resource.updated_at,
         )
         connection.execute(stmt)
 
