0ea7da1b5893bf7897741693c4a1fc20c16f20cf
Shinwoo Kim <sk274@cornell.edu>
2025-08-04 09:40:31 -0400

    Fix ruff error

--- bluecore_models_migration/versions/ad784bf46c6f_create_type_index.py
+++ bluecore_models_migration/versions/ad784bf46c6f_create_type_index.py
@@ -5,21 +5,21 @@ Revises: 20250728
 Create Date: 2025-08-01 10:30:52.579736
 
 """
+
 from typing import Sequence, Union
 
 from alembic import op
-import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision: str = 'ad784bf46c6f'
-down_revision: Union[str, None] = '20250728'
+revision: str = "ad784bf46c6f"
+down_revision: Union[str, None] = "20250728"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
 
 def upgrade() -> None:
-     # Create index on type
+    # Create index on type
     op.execute(
         """
         CREATE INDEX IF NOT EXISTS type_idx
