78e06fdb09615fbc6e971548b1469e12901a1cc7
Jeremy Duncan <jpd294@cornell.edu>
2025-09-30 13:09:04 -0400

    46: ruff formatting

--- src/bluecore_models/models/version.py
+++ src/bluecore_models/models/version.py
@@ -8,7 +8,9 @@ from bluecore_models.models.base import Base
 from bluecore_models.models.resource import ResourceBase
 
 from contextvars import ContextVar
+
 CURRENT_USER_ID: ContextVar[str | None] = ContextVar("current_user_id", default=None)
+
 
 class Version(Base):
     __tablename__ = "versions"
@@ -19,7 +21,9 @@ class Version(Base):
     )
     resource: Mapped[ResourceBase] = relationship("ResourceBase", backref="versions")
     data: Mapped[bytes] = mapped_column(JSONB, nullable=False)
-    keycloak_user_id: Mapped[str | None] = mapped_column(String(128), index=True, nullable=True)
+    keycloak_user_id: Mapped[str | None] = mapped_column(
+        String(128), index=True, nullable=True
+    )
     created_at = mapped_column(DateTime, default=datetime.utcnow)
 
     def __repr__(self):
